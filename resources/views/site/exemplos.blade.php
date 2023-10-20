777d880b3c4c765f1e1c75e7c1b90e123ebc579d
André Felix <dev.andresilva@gmail.com>
2023-10-20 12:08:44 -0300

    Correções pra funcionar no LT

--- resources/views/site/exemplos.blade.php
+++ resources/views/site/exemplos.blade.php
@@ -1,5 +1,41 @@
+@extends('site.layout')
+@section('title', 'HOME')
+@section('conteudo')
 <h1>Empresa</h1>
 
 Meu nome é {{$nome}} e minha idade é {{$idade}}.
 
 {!! $html !!}
+
+<h1>Aqui o base está sendo herdado de layout.blade.php, o section é um bloco chamado conteúdo que está sendo inserido dentro do campo yield do arquivo 'layout.blade.php'.</h1>
+
+{{-- Isso é um comentário --}}
+
+{{ isset($nome) ? 'Existe' : 'Não existe' }}
+
+<br>
+@if ($nome == 'André')
+true
+@else
+false
+@endif
+<br>
+@for ($i = 0; $i < 10; $i++)
+valor atual é {{ $i }} <br>
+@endfor
+<br>
+@php $i = 0; @endphp
+@while ($i<=10)
+valor atual usando while é {{ $i }} <br>
+@php $i++ @endphp
+@endwhile
+
+@include('includes.mensagem', ['titulo'=>'Mensagem de sucesso!'])
+
+@component('components.sidebar')
+@slot('paragrafo')
+Texto qualquer vindo do slot.
+@endslot
+@endcomponent
+
+@endsection
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,35 +1,7 @@
 @extends('site.layout')
 @section('title', 'HOME')
 @section('conteudo')
-    <h1>Aqui o base está sendo herdado de layout.blade.php, o section é um bloco chamado conteúdo que está sendo inserido dentro do campo yield do arquivo 'layout.blade.php'.</h1>
 
-    {{-- Isso é um comentário --}}
-
-    {{ isset($nome) ? 'Existe' : 'Não existe' }}
-
-<br>
-@if ($nome == 'André')
-    true
-@else
-    false
-@endif
-<br>
-@for ($i = 0; $i < 10; $i++)
-    valor atual é {{ $i }} <br>
-@endfor
-<br>
-@php $i = 0; @endphp
-@while ($i<=10)
-valor atual usando while é {{ $i }} <br>
-@php $i++ @endphp
-@endwhile
-
-@include('includes.mensagem', ['titulo'=>'Mensagem de sucesso!'])
-
-@component('components.sidebar')
-@slot('paragrafo')
-    Texto qualquer vindo do slot.
-@endslot
-@endcomponent
+<h1>Essa é nossa home</h1>
     
 @endsection
