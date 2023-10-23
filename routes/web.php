f7356f4a487a29e3a7ce8e9b821f4ccf820f7b6d
André Felix <dev.andresilva@gmail.com>
2023-10-23 10:36:06 -0300

    Listagem de um unico produto (detail)

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ use App\Http\Controllers\SiteController;
 Route::resource('produtos', ProdutoController::class);
 
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
+
+Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
