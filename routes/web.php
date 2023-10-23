2c0d7fd2514c7457feabd53c0e0fec47b630041b
André Felix <dev.andresilva@gmail.com>
2023-10-23 15:13:31 -0300

    Carrinho

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,6 @@ Route::resource('produtos', ProdutoController::class);
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
 Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
+
 Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
+Route::post('/carrinho', [CarrinhoController::class, 'adicionaCarrinho'])->name('site.addcarrinho');
