12a9bc9a4f32c112159c8ad2d15307bf695d58bd
André Felix <dev.andresilva@gmail.com>
2023-10-23 12:49:34 -0300

    Configurando o carrinho. Criando o CarrinhoController

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\CarrinhoController;
 
 
 #EXEMPLOS DE ROTAS ESTÃO NO ARQUIVO routes/rotasteste.php
 
 Route::resource('produtos', ProdutoController::class);
-
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
-
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
-
 Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
+Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
