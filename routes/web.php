289cb7249342b9a9d8cc30cbceeea3cc76a8785a
André Felix <dev.andresilva@gmail.com>
2023-10-23 10:25:09 -0300

    Criação do SiteController e da Rota.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\SiteController;
 
 
 #EXEMPLOS DE ROTAS ESTÃO NO ARQUIVO routes/rotasteste.php
 
 Route::resource('produtos', ProdutoController::class);
+
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
