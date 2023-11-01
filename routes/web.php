e5f9e51eee9b92582524a823c3463e4935f96b46
André Felix <dev.andresilva@gmail.com>
2023-11-01 14:54:40 -0300

    Criando o create usuario

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\SiteController;
 use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 
 
 #EXEMPLOS DE ROTAS ESTÃO NO ARQUIVO routes/rotasteste.php
 
 Route::resource('produtos', ProdutoController::class);
+Route::resource('users', UserController::class);
+
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
 Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
 Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
@@ -24,6 +27,7 @@ Route::get('/limpar', [CarrinhoController::class, 'limpaCarrinho'])->name('site.
 Route::view('/login', 'login.form')->name('login.form');
 Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
 Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+Route::get('/register', [LoginController::class, 'create'])->name('login.create');
 
 #Nessa rota abaixo o middleware está sendo usado no controller através do construtor.
 #Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
