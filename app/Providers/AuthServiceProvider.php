cc3efc3571d46d2386ed9150e7d92b182964d748
André Felix <dev.andresilva@gmail.com>
2023-11-07 08:43:57 -0300

    Autorizacao com Gates.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
+use App\Models\Produto;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('ver-produto', function(User $user, Produto $produto){
+            return $user->id == $produto->id_user;
+        });
     }
 }
