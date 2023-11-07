945435f4675f122755c7dd5398beedbddd9aab11
André Felix <dev.andresilva@gmail.com>
2023-11-07 09:22:36 -0300

    Permissao no botao pra ver o produto.

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -10,8 +10,11 @@
                 <div class="card-image">
                 <img src="{{ $produto->imagem }}">
                 
+                @can('verProduto', $produto)
                 <a href="{{ route('site.details', $produto->slug) }}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
-                </div>
+                @endcan
+
+            </div>
                 <div class="card-content">
                     <span class="card-title">{{ $produto->nome }}</span>
                     <p>{{ Str::limit($produto->descricao , 20) }}</p>
